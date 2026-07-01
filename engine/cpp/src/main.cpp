f11eab8906917ef6fe28e8ea61973efaeb722798
unknown <olawozny14@icloud.com>
2026-07-01 23:09:46 +0200

    update main

--- engine/cpp/src/main.cpp
+++ engine/cpp/src/main.cpp
@@ -1,32 +1,40 @@
-#include "../include/png_decoder.hpp"
-#include "../include/vision.hpp"
+#include "matrix.hpp"
+#include "geometry.hpp"
 #include <iostream>
 #include <vector>
+#include <string>
 
-int main() {
-    const char* input_file = "assets/test_image.png"; 
-    const char* output_file = "assets/output_edges.png";
-    
-    engine::vision::PngInfo image_info;
-    std::vector<uint8_t> raw_pixels;
-    
-    // Extract the raw pixels using stb_image (via png_decoder)
-    if (!engine::vision::load_png_pixels(input_file, image_info, raw_pixels)) {
-        return -1; // Stop if we can't find the image
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 7) {
+        cout << "Error: Expected 6 coordinates, but got " << (argc - 1) << endl;
+        return -1;
     }
 
-    // Bridge the pixels into a float matrix
-    Matrix gray_img = image_to_matrix(raw_pixels, image_info.width, image_info.height);
-    
-    // Run the edge detection 
-    float threshold = 0.5f; 
-    Matrix edge_map = edge_detection(gray_img, threshold);
-  
-    
-    // Save the final matrix back to a file using stb_image_write
-    if (matrix_to_image(edge_map, output_file)) {
-        std::cout << "See assets folder for output." << std::endl;
+    float hip_x   = stof(argv[1]);
+    float hip_y   = stof(argv[2]);
+    float knee_x  = stof(argv[3]);
+    float knee_y  = stof(argv[4]);
+    float ankle_x = stof(argv[5]);
+    float ankle_y = stof(argv[6]);
+
+    float angle = calculate_knee_angle(hip_x, hip_y, knee_x, knee_y, ankle_x, ankle_y);
+
+    cout << "\nANALYSIS" << endl;
+    cout << "Knee Angle: " << angle << " degrees" << endl;
+
+    if (angle <= 100.0f) {
+        cout << "Depth: EXCELLENT!" << endl;
+    } 
+
+    else if (angle <= 120.0f) {
+        cout << "Depth: GOOD! Parallel." << endl;
+    } 
+
+    else {
+        cout << "Depth: TOO HIGH!" << endl;
     }
-    
+
     return 0;
 }
