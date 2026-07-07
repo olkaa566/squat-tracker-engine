d00355406582ac2b488106482ef21deb4314800b
unknown <olawozny14@icloud.com>
2026-07-07 07:02:10 +0200

    add movement tracking with YOLOv8-POSE

---
+++ engine/py/src/tracker.py
@@ -0,0 +1,54 @@
+import cv2
+import subprocess
+from ultralytics import YOLO
+
+print("Booting up YOLOv8 AI.")
+model = YOLO('yolov8m-pose.pt') 
+
+cap = cv2.VideoCapture("assets/test_video.mp4")
+
+if not cap.isOpened():
+    print("Error: Could not open video file.")
+    exit()
+
+max_hip_y = -1.0
+lowest_hip_x, lowest_hip_y = 0.0, 0.0
+lowest_knee_x, lowest_knee_y = 0.0, 0.0
+
+print("Tracking started.")
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    results = model(frame, verbose=False)
+
+    if results[0].keypoints is not None and len(results[0].keypoints.xyn) > 0:
+        keypoints = results[0].keypoints.xyn.cpu().numpy()[0]
+       
+        if len(keypoints) >= 15: 
+            hip_x, hip_y = keypoints[12]
+            knee_x, knee_y = keypoints[14]
+
+            if hip_y > 0 and knee_y > 0:
+                if hip_y > max_hip_y:
+                    max_hip_y = hip_y
+                    lowest_hip_x, lowest_hip_y = hip_x, hip_y
+                    lowest_knee_x, lowest_knee_y = knee_x, knee_y
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("YOLOv8 Squat Tracker", annotated_frame)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+print("Fnished.")
+terminal_command = ['engine/cpp/build/Debug/engine.exe',str(lowest_hip_x), str(lowest_hip_y),str(lowest_knee_x), str(lowest_knee_y)]
+
+subprocess.run(terminal_command, shell=True)
